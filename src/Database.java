3af47763330280cd97e19d25745cb124d9b0c0e8
Arran Reedman <inventor.agr@btinternet.com>
2020-01-07 19:27:05 +0000

    Checkstyle + Javadoc changes

--- src/Database.java
+++ src/Database.java
@@ -8,10 +8,16 @@ public class Database {
   private String url;
   private Connection conn;
   
-  public Database(String url){
+  public Database(String url) {
     this.url = url;
   }
   
+  /**
+   * Logs into the database.
+   * @param username to login
+   * @param password to login
+   * @return result
+   */
   public boolean login(String username, String password) {
     try {
       conn = DriverManager.getConnection(url, username, password);
@@ -25,13 +31,17 @@ public class Database {
   public boolean isConnected() {
     return (conn != null);
   }
-
-  public void createTable(String tableName, String[][] headings){
+  
+  /**
+   * Creates a database table.
+   * @param tableName to create
+   * @param headings to use
+   */
+  public void createTable(String tableName, String[][] headings) {
     try {
-      Statement st = conn.createStatement();
       String sql = "CREATE TABLE " + tableName + " (";
-      for (int i = 0; i<headings.length;i++) {
-        for (int j = 0; j <headings[i].length;j++) {
+      for (int i = 0; i < headings.length; i++)  {
+        for (int j = 0; j < headings[i].length; j++) {
           if (i != 0 || j != 0) {
             sql += " ";
           }
@@ -41,7 +51,7 @@ public class Database {
       }
       sql = sql.substring(0, sql.length() - 1);
       sql += ")";
-
+      Statement st = conn.createStatement();
       st.execute(sql);
       st.close();
     } catch (SQLException e) {
@@ -52,10 +62,14 @@ public class Database {
     }
   }
 
+  /**
+   * Removes a table.
+   * @param table name to remove
+   */
   public void dropTable(String table) {
     try {
       Statement st = conn.createStatement();
-      st.execute("DROP TABLE IF EXISTS "+ table);
+      st.execute("DROP TABLE IF EXISTS " +  table);
       st.close();
     } catch (SQLException e) {
       e.printStackTrace();
@@ -64,11 +78,16 @@ public class Database {
     }
   }
 
+  /**
+   * Inserts a row of data into a table.
+   * @param tableName to insert into
+   * @param headings being inserted
+   * @param row being inserted
+   */
   public void insertRow(String tableName,String[] headings, String[] row) {
     try {
-      Statement st = conn.createStatement();
       String sql = "INSERT INTO " + tableName + " (";
-      for (int i = 0; i<headings.length;i++) {
+      for (int i = 0; i < headings.length; i++) {
         if (i != 0) {
           sql += ", ";
         }
@@ -76,19 +95,19 @@ public class Database {
       }
           
       sql += ") VALUES (";
-      for (int i = 0; i<row.length;i++) {
+      for (int i = 0; i < row.length; i++) {
         if (i != 0) {
           sql += ", ";
         }
         if (row[i] == "null") {
           sql += row[i];
-        }
-        else {
-          sql += "'" +row[i] + "'";
+        } else {
+          sql += "'" + row[i] + "'";
         }
       }
       sql += ")";
       //System.out.println(sql);
+      Statement st = conn.createStatement();
       st.executeUpdate(sql);
       st.close();
     } catch (SQLException e) {
@@ -97,13 +116,27 @@ public class Database {
     }
   }
 
+  /**
+   * Inserts multiple rows into table.
+   * @param tableName to insert into
+   * @param headers to insert
+   * @param rows to insert
+   */
   public void insertRows(String tableName, String[] headers, String[][] rows) {
     for (int i = 0; i < rows.length; i++) {
       insertRow(tableName, headers, rows[i]);
     }
   }
   
-  public String[][] selectQuery(String select, String from, String where, String post){
+  /**
+   * Queries the database with a select query.
+   * @param select portion
+   * @param from portion
+   * @param where portion
+   * @param post portion
+   * @return result
+   */
+  public String[][] selectQuery(String select, String from, String where, String post) {
     String sql = "SELECT " + select + " FROM " + from + " WHERE " + where + " " + post;
     return query(sql);
     
@@ -112,11 +145,12 @@ public class Database {
   private String[][] query(String sql) {
     try {
       //System.out.println(sql);
-      Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+      Statement st = conn.createStatement(
+          ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
       if (st.execute(sql)) {
         String[][] result = processResultSet(st.getResultSet());
         return result;
-      }else {
+      } else {
         return null;
       }
     } catch (SQLException e) {
@@ -135,14 +169,14 @@ public class Database {
         rs.beforeFirst();
       }
       String[][] contents = new String[rows][columns];
-      for (int j = 1; rs.next() && j<=rows;j++) {
+      for (int j = 1; rs.next() && j <= rows; j++) {
         for (int i = 1; i <= columns; i++) {
-          contents[j-1][i-1] = rs.getString(i);
+          contents[j - 1][i - 1] = rs.getString(i);
         }
       }
       rs.close();
       return contents;
-    }catch (SQLException e) {
+    } catch (SQLException e) {
       e.printStackTrace();
       System.exit(0);
     }
--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,45 @@ import java.util.Scanner;
 
 public class Main {
   private static int queryNumber = 1;
+  
+  /**
+   * Main method.
+   * @param args initialised with
+   */
   public static void main(String[] args) {
     Loader loader = new Loader();
     Scanner scan = new Scanner(System.in);
     loader.loadUrls();
     loader.loadMappings();
-    Boolean debug = false;
     Database db;
     do {
       System.out.println("Enter database to connect to (of format jdbc:postgresql://<url>)");
       System.out.print("> ");
-      String database_address = scan.nextLine();
-      if (database_address.equals("debug")) {
-        debug = true;
+      String databaseAddress = scan.nextLine();
+      if (databaseAddress.equals("debug")) {
         db = new Database("jdbc:postgresql://localhost/CS2855");
         db.login("test", "password");
-      }else {
+      } else {
         System.out.println("Enter database username");
         System.out.print("> ");
         String username = scan.nextLine();
-        
-        System.out.println("Enter database password");
-        System.out.print("> ");
+        System.out.print("Enter database password\n> ");
         String password = scan.nextLine();
         
-        db = new Database(database_address);
+        db = new Database(databaseAddress);
         if (!db.login(username, password)) {
           System.out.println("Connection failed to establish, please check and try again\n");
         }
       }
-    }while(!db.isConnected());
-    
-    if(db.isConnected()) {
+    } while (!db.isConnected());
+    scan.close();
+    if (db.isConnected()) {
       setup(db, loader);
-      String[][] result = db.selectQuery("rankings.ranking,websites.domain,websites.sub_dot,dots.text",
+      String[][] result = db.selectQuery(
+          "rankings.ranking,websites.domain,websites.sub_dot,dots.text",
           "websites,rankings,dots",
-          "rankings.ws_id=websites.ws_id AND websites.last_dot=dots.dot_id AND rankings.ranking <= 10",
+          "rankings.ws_id=websites.ws_id AND"
+          + "websites.last_dot=dots.dot_id AND rankings.ranking <= 10",
           "ORDER BY rankings.ranking ASC");
       display(result);
       
@@ -49,25 +52,25 @@ public class Main {
       
       result = db.selectQuery("dots.meaning",
           "websites,rankings,dots",
-          "rankings.ws_id=websites.ws_id AND websites.last_dot=dots.dot_id AND dots.meaning!='Not supplied'",
+          "rankings.ws_id=websites.ws_id AND"
+          + "websites.last_dot=dots.dot_id AND dots.meaning!='Not supplied'",
           "GROUP BY dots.meaning ORDER BY MIN(rankings.ranking) LIMIT 10");
       display(result);
       
       result = db.selectQuery("websites.domain",
           "websites,rankings,dots",
           "rankings.ws_id=websites.ws_id AND websites.last_dot=dots.dot_id",
-          "GROUP BY websites.domain HAVING COUNT(websites.domain)>1 ORDER BY MIN(rankings.ranking) LIMIT 10");
+          "GROUP BY websites.domain"
+          + "HAVING COUNT(websites.domain)>1 ORDER BY MIN(rankings.ranking) LIMIT 10");
       display(result);
       
-    }else {
-      int i;
     }
   }
 
   private static void display(String[][] data) {
-    System.out.println("################# Query "+ queryNumber++ +" ################");
-    for (int i=0; i < data.length; i++) {
-      for (int j=0; j < data[i].length; j++) {
+    System.out.println("################# Query " + queryNumber++ + " ################");
+    for (int i = 0; i < data.length; i++) {
+      for (int j = 0; j < data[i].length; j++) {
         if (!data[i][j].equals("null")) {
           System.out.print(data[i][j] + "\t");
         }
@@ -78,17 +81,7 @@ public class Main {
   }
 
   private static void setup(Database db, Loader loader) {
-    String[][] website_columns = {
-        {"ws_id","SERIAL", "PRIMARY KEY"}, 
-        {"domain","VARCHAR(50)"},
-        {"sub_dot","VARCHAR(11)"},
-        {"last_dot","INT REFERENCES dots(dot_id)"}
-    };
-    String[][] ranking_columns = {
-        {"ranking","INT", "PRIMARY KEY"}, 
-        {"ws_id","SERIAL REFERENCES websites(ws_id)"}
-    };
-    String[][] dot_columns = {
+    String[][] dotColumns = {
         {"dot_id","SERIAL", "PRIMARY KEY"}, 
         {"text","VARCHAR(10)"},
         {"meaning","VARCHAR(255)"}
@@ -97,58 +90,73 @@ public class Main {
     db.dropTable("websites");
     db.dropTable("dots");
     
-    db.createTable("dots", dot_columns);
-    String[] dot_headings = {"text", "meaning"};
-    db.insertRows("dots", dot_headings, loader.getMappings());
+    db.createTable("dots", dotColumns);
+    String[] dotHeadings = {"text", "meaning"};
+    db.insertRows("dots", dotHeadings, loader.getMappings());
     
-    String[][] website_data_combined = loader.getUrls();
-    String[][] website_data = new String[website_data_combined.length][3];
-    String[][] ranking_data = new String[website_data_combined.length][1];
-    for (int i = 0; i < website_data_combined.length; i++) {
-      for (int j=0; j < website_data_combined[i].length; j++) {
-        switch(j) {
-        case 0:
-          ranking_data[i][0] = website_data_combined[i][j];
-          break;
-        case 1:
-          website_data[i][0] = website_data_combined[i][j];
-          break;
-        case 2:
-          if(website_data_combined[i].length == 3) {
-            String[][] id = db.selectQuery("dot_id", "dots", "text = '" + website_data_combined[i][j] +"'", "");
-            if (id.length == 0) {
-              String[] data = {website_data_combined[i][j], "Not supplied"};
-              db.insertRow("dots", dot_headings, data);
-              website_data[i][2] = db.selectQuery("dot_id", "dots", "text = '" + website_data_combined[i][j] +"'", "")[0][0];
-            }else {
-              website_data[i][2] = id[0][0];
+    String[][] websiteDataCombined = loader.getUrls();
+    String[][] websiteData = new String[websiteDataCombined.length][3];
+    String[][] rankingData = new String[websiteDataCombined.length][1];
+    for (int i = 0; i < websiteDataCombined.length; i++) {
+      for (int j = 0; j < websiteDataCombined[i].length; j++) {
+        switch (j) {
+          case 0:
+            rankingData[i][0] = websiteDataCombined[i][j];
+            break;
+          case 1:
+            websiteData[i][0] = websiteDataCombined[i][j];
+            break;
+          case 2:
+            if (websiteDataCombined[i].length == 3) {
+              String[][] id = db.selectQuery("dot_id", "dots", "text = '"
++ websiteDataCombined[i][j] + "'", "");
+              if (id.length == 0) {
+                String[] data = {websiteDataCombined[i][j], "Not supplied"};
+                db.insertRow("dots", dotHeadings, data);
+                websiteData[i][2] = db.selectQuery("dot_id", "dots", "text = '"
+                + websiteDataCombined[i][j] + "'", "")[0][0];
+              } else {
+                websiteData[i][2] = id[0][0];
+              }
+            } else {
+              websiteData[i][1] = websiteDataCombined[i][j];
             }
-          }else {
-            website_data[i][1] = website_data_combined[i][j];
-          }
-          break;
-        case 3:
-          String[][] id = db.selectQuery("dot_id", "dots", "text = '" + website_data_combined[i][j] +"'", "");
-          if (id.length == 0) {
-            String[] data = {website_data_combined[i][j], "Not supplied"};
-            db.insertRow("dots", dot_headings, data);
-            website_data[i][2] = db.selectQuery("dot_id", "dots", "text = '" + website_data_combined[i][j] +"'", "")[0][0];
-          }else {
-            website_data[i][2] = id[0][0];
-          }
-          
+            break;
+          case 3:
+            String[][] id = db.selectQuery("dot_id", "dots", "text = '"
+          + websiteDataCombined[i][j] + "'", "");
+            if (id.length == 0) {
+              String[] data = {websiteDataCombined[i][j], "Not supplied"};
+              db.insertRow("dots", dotHeadings, data);
+              websiteData[i][2] = db.selectQuery("dot_id", "dots", "text = '"
+              + websiteDataCombined[i][j] + "'", "")[0][0];
+            } else {
+              websiteData[i][2] = id[0][0];
+            }
+            break;
+          default:
+            break;
         }
       }
     }
+    String[][] websiteColumns = {
+        {"ws_id","SERIAL", "PRIMARY KEY"}, 
+        {"domain","VARCHAR(50)"},
+        {"sub_dot","VARCHAR(11)"},
+        {"last_dot","INT REFERENCES dots(dot_id)"}
+    };
+    String[][] rankingColumns = {
+        {"ranking","INT", "PRIMARY KEY"}, 
+        {"ws_id","SERIAL REFERENCES websites(ws_id)"}
+    };
     
+    db.createTable("websites", websiteColumns);
+    String[] wsHeadings = {"domain", "sub_dot", "last_dot"};
+    db.insertRows("websites", wsHeadings, websiteData);
     
-    db.createTable("websites", website_columns);
-    String[] ws_headings = {"domain", "sub_dot", "last_dot"};
-    db.insertRows("websites", ws_headings, website_data);
-    
-    db.createTable("rankings", ranking_columns);
+    db.createTable("rankings", rankingColumns);
     String[] headings = {"ranking"};
-    db.insertRows("rankings", headings, ranking_data);
+    db.insertRows("rankings", headings, rankingData);
     
   }
 
